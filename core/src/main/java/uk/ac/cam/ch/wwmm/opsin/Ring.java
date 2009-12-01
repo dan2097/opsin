7ee25a168350bb4ef28e82a749b57fe559f1b0b5
Daniel Lowe <731646+dan2097@users.noreply.github.com>
2009-12-01 18:46:14 +0000

    Utilise Sam's SSSR code Tidyed up a few things and used Lists rather than Arraylists

--- core/src/main/java/uk/ac/cam/ch/wwmm/opsin/Ring.java
+++ core/src/main/java/uk/ac/cam/ch/wwmm/opsin/Ring.java
@@ -1,118 +1,99 @@
 package uk.ac.cam.ch.wwmm.opsin;
 
 import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Class representing a single ring (i.e. NOT a fused ring which is formed from multiple rings)
+ * @author dl387
+ *
+ */
 class Ring {
-	private ArrayList<Atom>  atomSet;
-	private ArrayList<Bond>  bondSet;
-	private ArrayList<Atom>  cyclicAtomSet;
-	private ArrayList<Bond>  cyclicBondSet;
-	private ArrayList<Ring>  neighbors = new ArrayList<Ring>();
+	private List<Atom>  atomSet;
+	private List<Bond>  bondSet;
+	private List<Atom>  cyclicAtomSet;
+	private List<Bond>  cyclicBondSet;
+	private List<Ring>  neighbours = new ArrayList<Ring>();
 
 	private int nFusedBonds = 0;
 
-	private int size = -1;
-
-	public Ring(ArrayList<Atom> atomSet, ArrayList<Bond> bondSet) throws StructureBuildingException {
+	Ring(List<Atom> atomSet) throws StructureBuildingException {
 		this.atomSet = atomSet;
-		this.bondSet = bondSet;
-		if (atomSet.size() != bondSet.size()) {
-			throw new StructureBuildingException("atomSet and bondset different sizes");
+		List<Bond> orderedBonds = new ArrayList<Bond>();
+		RingAtomIterator ringIteratorFromAtom0 = new RingAtomIterator(atomSet, 0);
+		RingAtomIterator ringIteratorFromAtom1 = new RingAtomIterator(atomSet, 1);
+		while (ringIteratorFromAtom0.hasNext()) {
+			Atom a1 = ringIteratorFromAtom0.next();
+			Atom a2 = ringIteratorFromAtom1.next();
+			orderedBonds.add(a1.getFrag().findBondOrThrow(a1, a2));
 		}
+		this.bondSet = orderedBonds;
 	}
 
-	public Ring(ArrayList<Bond> bondSet) throws StructureBuildingException {
-		if (bondSet==null || bondSet.size()<=0) throw new StructureBuildingException("Bond set is empty");
-		this.bondSet = bondSet;
-		this.atomSet = new ArrayList<Atom>();
 
-		for(Bond bond: bondSet)
-		{
-			Atom atom1 = bond.getFromAtom();
-			if (!atomSet.contains(atom1)) atomSet.add(atom1);
-
-			Atom atom2 = bond.getToAtom();
-			if (!atomSet.contains(atom2)) atomSet.add(atom2);
-
-		}
-
-		if (atomSet.size() != bondSet.size()) {
-			throw new StructureBuildingException("atomSet and bondset different sizes. Ring(bond)");
-		}
-
-	}
-
-	public ArrayList<Bond>  getBondSet() {
+	List<Bond>  getBondSet() {
 		return bondSet;
 	}
 
-	public ArrayList<Atom>  getAtomSet() {
+	List<Atom>  getAtomSet() {
 		return atomSet;
 	}
 
-	public int size() {
-		if (size<0)	size = bondSet.size();
-		return size;
+	/**
+	 * Number of ring atoms/bonds
+	 * @return
+	 */
+	int size() {
+		return atomSet.size();
 	}
 
-	public int getNOFusedBonds() {
+	int getNumberOfFusedBonds() {
 		return nFusedBonds;
 	}
 
-	public void setNOFusedBonds(int fusedBonds) {
-		this.nFusedBonds = fusedBonds;
-	}
-
-	public void incrNOFFusedBonds() {
+	void incrementNumberOfFusedBonds() {
 		this.nFusedBonds++;
 	}
 
-	public ArrayList<Bond> getFusedBonds()
-	{
+	/**
+	 * Return bonds utilised in multiple rings
+	 * @return List<Bond>
+	 */
+	List<Bond> getFusedBonds(){
 		ArrayList<Bond> bonds = new ArrayList<Bond>();
 
 		for (Bond bond : bondSet) {
 			if (bond.getFusedRings().size()>0) bonds.add(bond);
 		}
-
 		return bonds;
 	}
 
-	public int getBondNumber(Bond bond)
-	{
-		int i=0;
-		for(Bond ibond : cyclicBondSet)
-		{
-			if (ibond == bond) return i;
-			i++;
-		}
-		return -1;
+	int getBondNumber(Bond bond){
+		return cyclicBondSet.indexOf(bond);
 	}
 
-	public ArrayList<Bond> getCyclicBondSet()
-	{
+	List<Bond> getCyclicBondSet(){
 		return cyclicBondSet;
 	}
 
-	public ArrayList<Atom> getCyclicAtomSet()
-	{
+	List<Atom> getCyclicAtomSet(){
 		return cyclicAtomSet;
 	}
 
-	public ArrayList<Ring> getNeighbors() {
-		return neighbors;
+	List<Ring> getNeighbours() {
+		return neighbours;
 	}
 
-	public void addNeighbor(Ring ring) {
-		this.neighbors.add(ring);
+	void addNeighbour(Ring ring) {
+		this.neighbours.add(ring);
 	}
+
 	/**
 	 * Stores atoms and bonds in the order defined by atom and bond
 	 * @param stBond - the first bond
 	 * @param stAtom - the atom defining in which direction to go
 	 */
-	public void makeCyclicSets(Bond stBond, Atom stAtom)
-	{
+	void makeCyclicSets(Bond stBond, Atom stAtom){
 		if (cyclicBondSet==null){
 			cyclicBondSet = new ArrayList<Bond>();
 			cyclicAtomSet = new ArrayList<Atom>();
@@ -121,19 +102,15 @@ class Ring {
 			cyclicBondSet.add(stBond);
 			cyclicAtomSet.add(atom);
 
-			for (int i=0; i<size()-1; i++)
-			{
-				for(Bond bond2 : bondSet)
-				{
+			for (int i=0; i<size()-1; i++){
+				for(Bond bond2 : bondSet){
 					if (cyclicBondSet.contains(bond2)) continue;
-					if (bond2.getFromAtom() == atom)
-					{
+					if (bond2.getFromAtom() == atom){
 						cyclicBondSet.add(bond2);
 						atom = bond2.getToAtom();
 						cyclicAtomSet.add(atom);
 					}
-					else if (bond2.getToAtom() == atom)
-					{
+					else if (bond2.getToAtom() == atom){
 						cyclicBondSet.add(bond2);
 						atom = bond2.getFromAtom();
 						cyclicAtomSet.add(atom);
@@ -143,19 +120,20 @@ class Ring {
 		}
 	}
 
-	public String toString()
-	{
+	public String toString() {
 		String st = "";
-		ArrayList<Atom> toPrint;
+		List<Atom> toPrint;
 
-		if (cyclicAtomSet != null) toPrint = cyclicAtomSet;
-		else toPrint = atomSet;
-		for (Atom atom : toPrint)
-		{
+		if (cyclicAtomSet != null) {
+			toPrint = cyclicAtomSet;
+		}
+		else {
+			toPrint = atomSet;
+		}
+		for (Atom atom : toPrint){
 			st+=atom.getID() + " ";
 		}
 		return st;
-		//return size + "";
 	}
 
 }
