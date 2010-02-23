8571822a80bdf26cbbb0b82432da742bdca2b93a
Daniel Lowe <731646+dan2097@users.noreply.github.com>
2010-02-23 13:30:04 +0000

    Update pom to support MAVEN site generation. Added index.apt with info about the v0.6.0 release Added cyanamide

--- core/src/main/java/uk/ac/cam/ch/wwmm/opsin/OpsinResult.java
+++ core/src/main/java/uk/ac/cam/ch/wwmm/opsin/OpsinResult.java
@@ -14,18 +14,37 @@ public class OpsinResult {
 		return structure;
 	}
 
+	/**
+	 * Returns an enum with values SUCCESS, WARNING and FAILURE
+	 * Currently warning is never used
+	 * @return OPSIN_RESULT_STATUS status
+	 */
 	public OPSIN_RESULT_STATUS getStatus() {
 		return status;
 	}
 
+	/**
+	 * Returns a message explaining why generation of a molecule from the name failed
+	 * This string will be blank when no problems were encountered
+	 * @return String message
+	 */
 	public String getMessage() {
 		return message;
 	}
 	
+	/**
+	 * Returns the chemical name that this OpsinResult was generated frm
+	 * @return String chemicalName
+	 */
 	public String getChemicalName() {
 		return chemicalName;
 	}
 
+	/**
+	 * Lazily evaluates and returns the CML corresponding to the molecule described by the name
+	 * If name generation failed i.e. the OPSIN_RESULT_STATUS is FAILURE then null is returned
+	 * @return Element cml
+	 */
 	public synchronized Element getCml() {
 		if (cml ==null && structure!=null){
 			try{
