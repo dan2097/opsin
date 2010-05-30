bd368c9297aab86fc5534155cef86ad0d4add58e
dan2097 <dan2097@localhost>
2010-05-30 19:00:26 +0100

    Fixed a bug where heteroatom replacement could cause a charge suffix to be ignored Added ylia/ida/uida hetereoatom replacement terms Added a few unit tests for heteroatom replacement

---
+++ core/src/test/java/uk/ac/cam/ch/wwmm/opsin/HeteroAtomReplacementTest.java
@@ -0,0 +1,58 @@
+package uk.ac.cam.ch.wwmm.opsin;
+import static junit.framework.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HeteroAtomReplacementTest {
+
+	FragmentManager fragManager;
+	Atom a;
+
+	@Before
+	public void setUp() throws Exception {
+		fragManager = new FragmentManager(new SMILESFragmentBuilder(), mock(CMLFragmentBuilder.class), new IDManager());
+		a = new Atom(0, "C", mock(Fragment.class));
+	}
+	
+	@Test
+	public void thia() throws StructureBuildingException{
+		fragManager.makeHeteroatom(a, "S", false);
+		assertEquals(0, a.getCharge());
+		assertEquals(0, a.getProtonsExplicitlyAddedOrRemoved());
+		assertEquals(2, StructureBuildingMethods.calculateSubstitutableHydrogenAtoms(a));
+	}
+
+	@Test
+	public void thionia() throws StructureBuildingException{
+		fragManager.makeHeteroatom(a, "[SH3+]", false);
+		assertEquals(1, a.getCharge());
+		assertEquals(1, a.getProtonsExplicitlyAddedOrRemoved());
+		assertEquals(3, StructureBuildingMethods.calculateSubstitutableHydrogenAtoms(a));
+	}
+	
+	@Test
+	public void sulfanylia() throws StructureBuildingException{
+		fragManager.makeHeteroatom(a, "[SH+]", false);
+		assertEquals(1, a.getCharge());
+		assertEquals(-1, a.getProtonsExplicitlyAddedOrRemoved());
+		assertEquals(1, StructureBuildingMethods.calculateSubstitutableHydrogenAtoms(a));
+	}
+	
+	@Test
+	public void sulfanida() throws StructureBuildingException{
+		fragManager.makeHeteroatom(a, "[SH-]", false);
+		assertEquals(-1, a.getCharge());
+		assertEquals(-1, a.getProtonsExplicitlyAddedOrRemoved());
+		assertEquals(1, StructureBuildingMethods.calculateSubstitutableHydrogenAtoms(a));
+	}
+	
+	@Test
+	public void sulfanuida() throws StructureBuildingException{
+		fragManager.makeHeteroatom(a, "[SH3-]", false);
+		assertEquals(-1, a.getCharge());
+		assertEquals(1, a.getProtonsExplicitlyAddedOrRemoved());
+		assertEquals(3, StructureBuildingMethods.calculateSubstitutableHydrogenAtoms(a));
+	}
+}
