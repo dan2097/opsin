b44f5ac9b1476ccfee9f1e8efcea91076f8a495e
Richard Apodaca <rich.apodaca@gmail.com>
2010-12-16 08:08:22 -0800

    Tokeniser: Factor out methods unrelated to state.

--- core/src/main/java/uk/ac/cam/ch/wwmm/opsin/Tokeniser.java
+++ core/src/main/java/uk/ac/cam/ch/wwmm/opsin/Tokeniser.java
@@ -1,14 +1,9 @@
 package uk.ac.cam.ch.wwmm.opsin;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import uk.ac.cam.ch.wwmm.opsin.ParseWord.WordType;
 
 /**
  * Uses OPSIN's DFA based grammar to break a name in to tokens with associated meanings ("annotations").
@@ -17,17 +12,15 @@ import uk.ac.cam.ch.wwmm.opsin.ParseWord.WordType;
  */
 class Tokeniser {
 	private final ParseRules parseRules;
-	private final static char endOfSubstituent = '\u00e9';
-	private final static char endOfMainGroup = '\u00e2';
-	private final static char endOfFunctionalTerm = '\u00FB';
-	private final Pattern matchCommaSpace = Pattern.compile(", ");
-	private final Pattern matchSpace = Pattern.compile(" ");
-	private final Pattern matchAcid = Pattern.compile("acid[\\]\\)\\}]*");
 	private final Pattern matchCasCollectiveIndex = Pattern.compile("([\\[\\(\\{]([1-9][0-9]?[cC][iI][, ]?)+[\\]\\)\\}])+|[1-9][0-9]?[cC][iI]", Pattern.CASE_INSENSITIVE );
 	private final Pattern matchCompoundWithPhrase = Pattern.compile("(compd\\. with|compound with|and) ", Pattern.CASE_INSENSITIVE );
 
 	Tokeniser(ParseRules parseRules) {
 		this.parseRules = parseRules;
+	}
+
+	public ParseRules getParseRules() {
+		return this.parseRules;
 	}
 
 	/**
@@ -124,7 +117,7 @@ class Tokeniser {
 			String backParsedName = pw.getWord() + result.getUnparsedName().substring(0, result.getUnparsedName().length() - backUninterpretableName.length());
 			if (backParsedName.length() > pw.getWord().length() && backParseTokens.size() > 0 && (backUninterpretableName.equals("") || backUninterpretableName.charAt(0) == ' ')) {//a word was interpretable
 				result.getParse().removeWord(pw);
-				List<ParseWord> parseWords = splitIntoParseWords(backParseTokens, backParsedName);
+				List<ParseWord> parseWords = WordTools.splitIntoParseWords(backParseTokens, backParsedName);
 				for (ParseWord parseWord : parseWords) {
 					result.getParse().addWord(parseWord);
 				}
@@ -141,7 +134,7 @@ class Tokeniser {
 	}
 
 	private void addParseWords(List<ParseTokens> parseTokens, String parsedName, Parse parse, boolean reverse) throws ParsingException {
-		List<ParseWord> parseWords = splitIntoParseWords(parseTokens, parsedName);
+		List<ParseWord> parseWords = WordTools.splitIntoParseWords(parseTokens, parsedName);
 
 		if (reverse) {
 			Collections.reverse(parseWords);//make this set of words back to front as well
@@ -194,291 +187,5 @@ class Tokeniser {
 			return false;
 		}
 		return true;
-	}
-
-	private List<ParseWord> splitIntoParseWords(List<ParseTokens> parseTokensList, String chemicalName) throws ParsingException {
-		List<ParseTokens> wellFormedParseTokens = new ArrayList<ParseTokens>();//these are all in the same word as would be expected
-		List<List<ParseTokens>> omittedWordParseTokensList = new ArrayList<List<ParseTokens>>();//these are grouped into words e.g. ethylchloride will have a list of parseTokens for the ethyl and chloride
-		omittedWordParseTokensList.add(new ArrayList<ParseTokens>());
-		omittedWordParseTokensList.add(new ArrayList<ParseTokens>());//only 1 space is allowed to be omitted
-		int longestFunctionalTermEncountered =0;//we want the longest functional term
-		int shortestNonFunctionalTermEncountered = Integer.MAX_VALUE;//and the shortest non functional term
-		for (ParseTokens parseTokens : parseTokensList) {
-			List<Character> annotations = parseTokens.getAnnotations();
-			List<List<Character>> chunkedAnnotations = chunkAnnotations(annotations);//chunked into mainGroup/substituent/functionalTerm
-			if (chunkedAnnotations.size()>1 && annotations.contains(endOfFunctionalTerm)) {//must be an omitted space as not allowed to have a functionalTerm and anything else
-				List<String> tokens = parseTokens.getTokens();
-				List<Character> newAnnotations = new ArrayList<Character>();
-				List<String> newTokens = new ArrayList<String>();
-				int annotPos = 0;
-				int wordCounter = 0;
-				for (List<Character> annotationList : chunkedAnnotations) {
-					boolean functionalTermNext = false;
-					if (annotationList.get(annotationList.size()-1).equals(endOfFunctionalTerm)) {
-						functionalTermNext = true;
-						if (newAnnotations.size()>0){//create a new parseTokens, unless nothing has been read yet e.g. in the case of poly
-							ParseTokens newParseTokens = new ParseTokens(newTokens, newAnnotations);
-							if (wordCounter >=2){
-								throw new ParsingException("Name appears to have 2 or more omitted spaces!");
-							}
-							int currentNonFunctionalTermLength = StringTools.stringListToString(newTokens, "").length();
-							if (currentNonFunctionalTermLength <= shortestNonFunctionalTermEncountered  && !omittedWordParseTokensList.get(wordCounter).contains(newParseTokens)){
-								if (currentNonFunctionalTermLength < shortestNonFunctionalTermEncountered) {
-									omittedWordParseTokensList.get(wordCounter).clear();
-									shortestNonFunctionalTermEncountered =currentNonFunctionalTermLength;
-								}
-								omittedWordParseTokensList.get(wordCounter).add(newParseTokens);
-							}
-							wordCounter++;
-							newAnnotations = new ArrayList<Character>();
-							newTokens = new ArrayList<String>();
-						}
-					}
-					for (Character annotation : annotationList) {
-						newAnnotations.add(annotation);
-						newTokens.add(tokens.get(annotPos));
-						annotPos++;
-					}
-					if (functionalTermNext){
-						ParseTokens newParseTokens = new ParseTokens(newTokens, newAnnotations);
-						if (wordCounter >=2){
-							throw new ParsingException("Name appears to have 2 or more omitted spaces!");
-						}
-						int currentFunctionalTermLength = StringTools.stringListToString(newTokens, "").length();
-						if (currentFunctionalTermLength >= longestFunctionalTermEncountered && !omittedWordParseTokensList.get(wordCounter).contains(newParseTokens)){
-							if (currentFunctionalTermLength > longestFunctionalTermEncountered) {
-								omittedWordParseTokensList.get(wordCounter).clear();
-								longestFunctionalTermEncountered =currentFunctionalTermLength;
-							}
-							omittedWordParseTokensList.get(wordCounter).add(newParseTokens);
-						}
-						wordCounter++;
-						newAnnotations = new ArrayList<Character>();
-						newTokens = new ArrayList<String>();
-					}
-				}
-				if (!newAnnotations.isEmpty()) {
-					ParseTokens newParseTokens = new ParseTokens(newTokens, newAnnotations);
-					if (wordCounter >= 2){
-						throw new ParsingException("Name appears to have 2 or more omitted spaces!");
-					}
-					int currentNonFunctionalTermLength = StringTools.stringListToString(newTokens, "").length();
-					if (currentNonFunctionalTermLength <= shortestNonFunctionalTermEncountered  && !omittedWordParseTokensList.get(wordCounter).contains(newParseTokens)){
-						if (currentNonFunctionalTermLength < shortestNonFunctionalTermEncountered) {
-							omittedWordParseTokensList.get(wordCounter).clear();
-							shortestNonFunctionalTermEncountered =currentNonFunctionalTermLength;
-						}
-						omittedWordParseTokensList.get(wordCounter).add(newParseTokens);
-					}
-					wordCounter++;
-				}
-			}
-			else{
-				wellFormedParseTokens.add(parseTokens);
-			}
-		}
-		List<ParseWord> parseWords = new ArrayList<ParseWord>();
-		if (!wellFormedParseTokens.isEmpty()) {
-			parseWords.add(new ParseWord(chemicalName, wellFormedParseTokens));
-		}
-		else{
-			for (List<ParseTokens> omittedWordParseTokens : omittedWordParseTokensList) {
-				parseWords.add(new ParseWord(StringTools.stringListToString(omittedWordParseTokens.get(0).getTokens(), ""), omittedWordParseTokens));
-			}
-		}
-		return parseWords;
-	}
-	
-	/**
-	 * Inverts a CAS name.
-	 * Throws an exception is OPSIN is unable to determine whether something is a substituent or functional term
-	 * or if something unexpected in a CAS name is encountered
-	 * @param name
-	 * @return
-	 * @throws ParsingException
-	 */
-	String uninvertCASName(String name) throws ParsingException {
-		List<String> nameComponents = new ArrayList<String>(Arrays.asList(matchCommaSpace.split(name)));
-		List<String> substituents = new ArrayList<String>();
-		List<String> seperateWordSubstituents = new ArrayList<String>();
-		List<String> functionalTerms = new ArrayList<String>();
-		
-		String parent = nameComponents.get(0);
-		String[] parentNameParts = matchSpace.split(parent);
-		if (parentNameParts.length !=1){
-			if (matchCasCollectiveIndex.matcher(parentNameParts[parentNameParts.length-1]).matches()){//CAS collective index description should be ignored
-				parent = "";
-				for (int i = 0; i < parentNameParts.length-1; i++) {
-					parent += parentNameParts[i];
-				}
-				parentNameParts = matchSpace.split(parent);
-			}
-			for (int i = 1; i < parentNameParts.length; i++) {
-				if (!matchAcid.matcher(parentNameParts[i]).matches()){
-					ParseRulesResults results = parseRules.getParses(parentNameParts[i]);
-					List<ParseTokens> parseTokens = results.getParseTokensList();
-					if (parseTokens.isEmpty()){
-						throw new ParsingException("Invalid CAS name. Parent compound was followed by an unexpected term");
-					}
-				}
-			}
-		}
-		boolean addedBracket = false;
-		boolean esterEncountered = false;
-		for (int i = 1; i < nameComponents.size(); i++) {
-			String nameComponent = nameComponents.get(i);
-			Matcher m = matchCompoundWithPhrase.matcher(nameComponent);
-			boolean compoundWithcomponent =false;
-			if (m.lookingAt()){
-				nameComponent = nameComponent.substring(m.group().length());
-				compoundWithcomponent =true;
-			}
-			String[] components = matchSpace.split(nameComponents.get(i));
-			for (String component : components) {
-				if (compoundWithcomponent){
-					functionalTerms.add(component);
-					continue;
-				}
-				if (component.endsWith("-")){
-					if (isCloseBracketMissing(component)){
-						if (addedBracket){
-							throw new ParsingException("Close bracket bracket appears to be missing");
-						}
-						parent += "]";
-						addedBracket =true;
-					}
-					substituents.add(component);
-				}
-				else{
-					ParseRulesResults results = parseRules.getParses(component);
-					List<ParseTokens> parseTokens = results.getParseTokensList();
-					if (parseTokens.size() >0){
-						if (splitIntoParseWords(parseTokens, component).size()>1){
-							throw new ParsingException("Missing space found in name prevents interpetation as CAS index name");
-						}
-						WordType wordType = OpsinTools.determineWordType(parseTokens.get(0).getAnnotations());
-						for (int j = 1; j < parseTokens.size(); j++) {
-							if (!wordType.equals(OpsinTools.determineWordType(parseTokens.get(j).getAnnotations()))){
-								throw new ParsingException(component + "can be interpeted in multiple ways. For the sake of precision OPSIN has decided not to process this as a CAS name");
-							}
-						}
-						if (wordType.equals(WordType.functionalTerm)){
-							if (component.equalsIgnoreCase("ester")){
-								if (esterEncountered){
-									throw new ParsingException("ester formation was mentioned more than once in CAS name!");
-								}
-								parent = uninvertEster(parent);
-								esterEncountered=true;
-							}
-							else{
-								functionalTerms.add(component);
-							}
-						}
-						else if (wordType.equals(WordType.substituent)){
-							seperateWordSubstituents.add(component);
-						}
-						else if (wordType.equals(WordType.full)){
-							if (StringTools.endsWithCaseInsensitive(component, "ate") || StringTools.endsWithCaseInsensitive(component, "ite")//e.g. Piperazinium, 1,1-dimethyl-, 2,2,2-trifluoroacetate hydrochloride
-									|| component.equalsIgnoreCase("hydrofluoride") || component.equalsIgnoreCase("hydrochloride") || component.equalsIgnoreCase("hydrobromide") || component.equalsIgnoreCase("hydroiodide")){
-								functionalTerms.add(component);
-							}
-							else{
-								throw new ParsingException("Unable to interpret: " + component +" (as part of a CAS index name)- A full word was encountered where a substituent or functionalTerm was expected");
-							}
-						}
-					}
-					else{
-						if (!matchCasCollectiveIndex.matcher(component).matches()){//CAS collective index description should be ignored
-							throw new ParsingException("Unable to interpret: " + component +" (as part of a CAS index name)");
-						}
-					}
-				}
-			}
-		}
-		StringBuilder casName = new StringBuilder();
-		for (String prefixFunctionalTerm : seperateWordSubstituents) {
-			casName.append(prefixFunctionalTerm);
-			casName.append(" ");
-		}
-		for (String substituent : substituents) {
-			casName.append(substituent);
-		}
-		casName.append(parent);
-		for (String functionalTerm : functionalTerms) {
-			casName.append(" ");
-			casName.append(functionalTerm);
-		}
-		return casName.toString();
-	}
-
-	/**
-	 * Modifies the name of the parent acid from ic to ate (or ous to ite)
-	 * hence allowing the formation of the uninverted ester
-	 * @param parent
-	 * @return
-	 * @throws ParsingException
-	 */
-	private String uninvertEster(String parent) throws ParsingException {
-		int len = parent.length();
-		if (len <9){
-			throw new ParsingException("Failed to uninvert CAS ester");
-		}
-		char lastChar = parent.charAt(len-1);
-		if (lastChar ==')' || lastChar ==']' || lastChar =='}'){
-			if (parent.substring(parent.length()-8).equalsIgnoreCase("ic acid)")){
-				parent = parent.substring(0, parent.length()-8) + "ate)";
-			}
-			else if (parent.substring(parent.length()-9).equalsIgnoreCase("ous acid)")){
-				parent = parent.substring(0, parent.length()-9) + "ite)";
-			}
-			else{
-				throw new ParsingException("Failed to uninvert CAS ester");
-			}
-		}
-		else{
-			if (parent.substring(parent.length()-7).equalsIgnoreCase("ic acid")){
-				parent = parent.substring(0, parent.length()-7) + "ate";
-			}
-			else if (parent.substring(parent.length()-8).equalsIgnoreCase("ous acid")){
-				parent = parent.substring(0, parent.length()-8) + "ite";
-			}
-			else{
-				throw new ParsingException("Failed to uninvert CAS ester");
-			}
-		}
-		return parent;
-	}
-
-	private boolean isCloseBracketMissing(String component) {
-		char[] characters = component.toCharArray();
-		for (int i = characters.length -1; i >=0; i--) {
-			char character = characters[i];
-			if (character =='(' || character =='[' || character =='{'){
-				return true;
-			}
-			if (character ==')' || character ==']' || character =='}'){
-				return false;
-			}
-		}
-		return false;
-	}
-	
-	/**Groups the token annotations for a given word into substituent/s and/or a maingroup and/or functionalTerm by
-	 * looking for the endOfSubstituent/endOfMainGroup/endOfFunctionalTerm annotations
-	 *
-	 * @param annots The annotations for a word.
-	 * @return A List of lists of annotations, each list corresponds to a substituent/maingroup/functionalTerm
-	 */
-	List<List<Character>> chunkAnnotations(List<Character> annots) {
-		LinkedList<List<Character>> chunkList = new LinkedList<List<Character>>();
-		List<Character> currentTerm = new ArrayList<Character>();
-		for (Character annot : annots) {
-			currentTerm.add(annot);
-			if (annot.equals(endOfSubstituent) || annot.equals(endOfMainGroup) || annot.equals(endOfFunctionalTerm)){
-				chunkList.add(currentTerm);
-				currentTerm = new ArrayList<Character>();
-			}
-		}
-		return chunkList;
 	}
 }
--- core/src/test/java/uk/ac/cam/ch/wwmm/opsin/AllTests.java
+++ core/src/test/java/uk/ac/cam/ch/wwmm/opsin/AllTests.java
@@ -23,7 +23,8 @@ import org.junit.runners.Suite.SuiteClasses;
 	StereochemistryTest.class,
 	StructureBuilderTest.class,
 	TokenizerTest.class,
-	VerifyFragmentsTest.class
+	VerifyFragmentsTest.class,
+	CASToolsTest.class
 })
 public class AllTests {
 
